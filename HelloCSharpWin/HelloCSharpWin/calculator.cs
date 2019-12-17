d73bf54fec41d1e0412a4c101e701a7116e23894
HyoungHoKim <dkdemfp2@naver.com>
2019-12-17 22:12:14 +0900

    CSharp study 2~5

---
+++ HelloCSharpWin/HelloCSharpWin/calculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calculator : Form
+    {
+
+        public int Result = 0;
+        public bool isNewNum = true;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
+        }
+
+        private void NumButton1_Click(object sender, EventArgs e)
+        {
+            SetNum("1");
+        }
+
+        private void NumButton2_Click(object sender, EventArgs e)
+        {
+            SetNum("2");
+        }
+
+        public void SetNum(string num)
+        {
+            if (isNewNum)
+            {
+                NumScreen.Text = num;
+                isNewNum = false;
+            }
+            else if (NumScreen.Text == "0")
+            {
+                NumScreen.Text = num;
+            }
+            else
+            {
+                NumScreen.Text = NumScreen.Text + num;
+            }
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(NumScreen.Text);
+            Result = Result + num;
+
+            NumScreen.Text = Result.ToString();
+            isNewNum = true;
+        }
+    }
+}
